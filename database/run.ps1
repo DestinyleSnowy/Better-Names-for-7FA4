bad2c411075926ff6b3729bf7dee0243bd71ac6b
DestinyleSnowy <destinylesnowyx@gmail.com>
2026-02-02 18:03:03 +0800

    Update 2026.02

--- database/run.ps1
+++ database/run.ps1
@@ -4,7 +4,7 @@ python -m venv venv
 pip install aiohttp beautifulsoup4 lxml
 
 # cookie
-$env:JX_COOKIE = 'connect.sid=...; io=...; sidebar_collapsed=false; event_filter=all; login=%5B%22board%22%2C%2283393899d725503cb8beae5b015b75df%22%5D'
+$env:JX_COOKIE = 'connect.sid=...; io=...; sidebar_collapsed=false; event_filter=all; login=%5B%22bozrd%22%2C%229e25bde06d5a564a75b905033c3f4940%22%5D'
 
 # run
 python main.py
